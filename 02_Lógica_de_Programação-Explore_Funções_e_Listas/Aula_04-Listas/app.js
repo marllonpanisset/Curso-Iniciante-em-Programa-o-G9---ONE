f9c89c98af7c41f134da6713751615b1e81c3b40
panisset.dev@gmail.com <panisset.dev@gmail.com>
2025-08-07 06:20:21 -0300

    adicionando imagens, configurance responsiveVoice

--- 02_Lógica_de_Programação-Explore_Funções_e_Listas/Aula_04-Listas/app.js
+++ 02_Lógica_de_Programação-Explore_Funções_e_Listas/Aula_04-Listas/app.js
@@ -6,11 +6,13 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
 }
 
 exibirMensagemInicial();
